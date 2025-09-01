f0b47f0130cb49092fc83f8f9bcb4527d22b984a
Thyago Porto <100157505+ThyagoPorto@users.noreply.github.com>
2025-09-01 15:28:52 -0300

    Update main.py
    Correção de código!

--- main.py
+++ main.py
@@ -1,22 +1,14 @@
 import os
-
 from flask import Flask, send_from_directory
-from user import db
-from user import user_bp
-from dashboard import dashboard_bp
+from dashboard import dashboard_bp # Esta linha fica, pois você tem o dashboard.py
 
 app = Flask(__name__, static_folder=os.path.join(os.path.dirname(__file__), 'static'))
 app.config['SECRET_KEY'] = 'asdf#FGSgvasgf$5$WGT'
 
-app.register_blueprint(user_bp, url_prefix='/api')
+# A linha que regista o user_bp foi removida.
 app.register_blueprint(dashboard_bp, url_prefix='/api/dashboard')
 
-# uncomment if you need to use database
-app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(os.path.dirname(__file__), 'database', 'app.db')}"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db.init_app(app)
-with app.app_context():
-    db.create_all()
+# O bloco inteiro da base de dados foi removido.
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
